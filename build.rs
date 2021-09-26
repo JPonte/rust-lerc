eb6cce14d8e6394ad9fc8ce6dfca84c8062668d4
Joao Ponte <jpe@plista.com>
2021-09-26 14:08:46 +0200

    Simple decode function

--- build.rs
+++ build.rs
@@ -6,33 +6,35 @@ use std::path::Path;
 
 
 fn main() {
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
+    #[cfg(windows)]
     println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("lerc-3.0\\bin\\windows").display());
 
-    println!("cargo:rustc-link-lib=Lerc");
+    #[cfg(target_os="linux")]
+    println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("lerc-3.0/bin/Linux").display());
 
-    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    #[cfg(target_os="macos")]{
+        env::set_var("LLVM_CONFIG_PATH", "/usr/local/opt/llvm/bin/llvm-config"); // For some reason it doesn't pick up the system envs
+        println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("lerc-3.0/bin/MacOS").display());
+    }
+
+    #[cfg(windows)]
+    let include_path = format!("-I{}", Path::new(&dir).join("lerc-3.0\\src\\LercLib\\include").display());
+
+    #[cfg(not(windows))]
+    let include_path = format!("-I{}", Path::new(&dir).join("lerc-3.0/src/LercLib/include").display());
+    
+    println!("cargo:rustc-link-lib=dylib={}", "Lerc");
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    // The bindgen::Builder is the main entry point
-    // to bindgen, and lets you build up options for
-    // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
         .header("wrapper.h")
-        .clang_arg(format!("-I{}", Path::new(&dir).join("lerc-3.0\\src\\LercLib\\include").display()))
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
+        .clang_arg(include_path)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,91 @@
 
 include!(concat!(env!("CARGO_MANIFEST_DIR"), "/bindings.rs"));
 
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Debug)]
+pub struct LercBlobInfo {
+    pub version: u32,
+    pub dataType: u32,
+    pub nDim: u32,
+    pub nCols: u32,
+    pub nRows: u32,
+    pub nBands: u32,
+    pub nValidPixels: u32,
+    pub blobSize: u32,
+    pub nMasks: u32,
+}
+
+impl LercBlobInfo {
+    fn new(vec: &Vec<u32>) -> LercBlobInfo {
+        LercBlobInfo {
+            version: vec[0],
+            dataType: vec[1],
+            nDim: vec[2],
+            nCols: vec[3],
+            nRows: vec[4],
+            nBands: vec[5],
+            nValidPixels: vec[6],
+            blobSize: vec[7],
+            nMasks: vec[8],
+        }
+    }
+}
+
+pub struct LercBlob {
+    pub info: LercBlobInfo,
+    pub data: Vec<f64>,
+}
+
+pub fn decode_lerc_file(mut f: File) -> LercBlob {
+    unsafe {
+        let mut buf = Vec::new();
+        let size = f.read_to_end(&mut buf).unwrap();
+
+        let mut infoArr = vec![0; 9];
+        let mut dataRangeArr = vec![0f64; 3];
+        lerc_getBlobInfo(
+            buf.as_ptr(),
+            size as u32,
+            infoArr.as_mut_ptr(),
+            dataRangeArr.as_mut_ptr(),
+            9,
+            3,
+        );
+        let blob_info = LercBlobInfo::new(&infoArr);
+
+        let mut valid_bytes = vec![0; blob_info.nValidPixels as usize];
+        let mut p_data = vec![0f64; blob_info.nValidPixels as usize];
+
+        lerc_decodeToDouble(
+            buf.as_ptr(),
+            blob_info.blobSize,
+            1,
+            valid_bytes.as_mut_ptr(),
+            blob_info.nDim as i32,
+            blob_info.nCols as i32,
+            blob_info.nRows as i32,
+            1,
+            p_data.as_mut_ptr(),
+        );
+        LercBlob {
+            info: blob_info,
+            data: p_data,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::mem;
 
     #[test]
-    fn lerc_stuff() {
-        unsafe {
-            
-        }
+    fn decode_blob() {
+        let file = File::open("example_file").unwrap();
+        let blob = decode_lerc_file(file);
+
+        println!("Info: {:?}", blob.data);
+        println!("Info: {:?}", blob.info);
     }
 }
